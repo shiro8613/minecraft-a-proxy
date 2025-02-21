ecb06007803d8d02f1fc8d6b1fd4c6b123c3b244
toms0910 <psp1070@gmail.com>
2025-02-22 04:50:28 +0900

    プロキシの修正

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,10 +2,10 @@ package proxy
 
 import (
 	"context"
-	"errors"
 	"io"
 	"log"
 	"net"
+	"runtime"
 	"slices"
 
 	"github.com/shiro8613/minecraft-a-proxy/config"
@@ -38,7 +38,7 @@ func (s *ProxyServer) Start(ctx context.Context, addr *net.TCPAddr) error {
 	for {
 		conn, err := l.AcceptTCP()
 		if err != nil {
-			if errors.Is(err, net.ErrClosed) {
+			if err == net.ErrClosed {
 				return nil
 			}
 			return err
@@ -55,36 +55,59 @@ func (s *ProxyServer) handler(ctx context.Context, c *net.TCPConn) {
 	ip, _, err := net.SplitHostPort(addr)
 	if err != nil {
 		log.Println("[ERROR] ", err)
+		s = nil
+		c = nil
+		runtime.GC()
+		return
 	}
 
 	if HasBannedIps(ip) {
 		c.Close()
 		log.Printf("[INFO] banned-ip %s blocked\n", ip)
+		s = nil
+		c = nil
+	
+		runtime.GC()
+		return
 	}
 
 	p := &Proxy{}
 	err = p.Start(ctx, c)
 	if err != nil && err != io.EOF {
+		p = nil
+		s = nil
+		c = nil
 		log.Println("[ERROR] ", err)
 	}
+
+	p = nil
+	s = nil
+	c = nil
+	runtime.GC()
 }
 
 type Proxy struct {}
 
-func (p *Proxy) Start(ctx context.Context, cConn *net.TCPConn) error {
+func (pr *Proxy) Start(ctx context.Context, cConn *net.TCPConn) error {
 	defer cConn.Close()
 
 	var eg errgroup.Group
 
 	var sConn *net.TCPConn
-	addr := cConn.RemoteAddr()
-	logged := 0
-	
+	closed, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	eg.Go(func() error {
+		defer cancel()
+
+		addr := cConn.RemoteAddr()
+		logged := 0	
 		buff := make([]byte, 0xFFFF)
+
 		for {
 			n, err := cConn.Read(buff)
 			if err != nil {
+				buff = nil
 				return err
 			}
 			b := buff[:n]
@@ -95,74 +118,118 @@ func (p *Proxy) Start(ctx context.Context, cConn *net.TCPConn) error {
 						p := &packet.HelloPacket{}
 						r, l, err := p.Read(b1)
 						if err != nil {
+							p = nil
+							b1 = nil
+							buff = nil
+							b = nil
 							return err
 						}
-						
+
 						if r {
 							server, ok := config.GetConfig().Servers[p.Hostname]
 							if !ok {
-								return cConn.Close()
-							}
-
-							if p.State == 1 {
-								log.Printf("[INFO] %s is ping\n", addr)
+								p = nil
+								b1 = nil
+								buff = nil
+								b = nil
+								return io.EOF
 							}
 
 							serverIP, err := net.ResolveTCPAddr("tcp", server)
 							if err != nil {
+								p = nil
+								b1 = nil
+								buff = nil
+								b = nil
+								serverIP = nil
 								return err
 							}
 
 							sConn, err = net.DialTCP("tcp", nil, serverIP)
 							if err != nil {
+								p = nil
+								b1 = nil
+								buff = nil
+								b = nil
+								serverIP = nil
+								sConn = nil
 								return err
+							}
+
+							serverIP = nil
+
+							if p.State == 1 {
+								log.Printf("[INFO] %s is ping\n", addr)
 							}
 
 							if l != nil {
 								log.Printf("[INFO] player is connected %s <- [%s]%s(%s)\n", server, addr.String(), l.Name, l.Uuid)
+								b1 = nil
+								p = nil
+								l = nil
 								logged = 2
 								goto NEXT	
 							}
 
 							logged = 1
+							b1 = nil
+
 							goto NEXT
+						} else if sConn == nil && !r {
+							b = nil
+							b1 = nil
+							l = nil
+							p = nil
+							return io.EOF
 						}
 					}
 
 					if logged == 1 {
 						p := &packet.LoginPacket{}
 						r, err := p.Read(b1)
-						if err != nil && err != io.EOF {
+						if err != nil {
+							buff = nil
+							b = nil
+							b1 = nil
+							p = nil
 							return err
 						}
 
 						if r {
 							log.Printf("[INFO] player is connected (parse failed) <- [%s]%s(%s)\n", addr.String(), p.Name, p.Uuid)
+							b1 = nil
+							p = nil
 							logged = 2
 							goto NEXT
 						}
 					}
+				} else if logged == 2 {
+					addr = nil
+					logged = 3
 				}
 			}
 
 			NEXT:
 				if sConn != nil {
-					n, err = sConn.Write(b)
+					_, err = sConn.Write(b)
 					if err != nil {
+						buff = nil
+						b = nil
 						return err
 					}
 				}
 		
+			b = nil
+
 			select {
-			case <- ctx.Done():
-				break
+			case <- closed.Done():
+				return nil
 			default:
-				continue
 			}
 		}
 	})
 	
-	eg.Go(func() error { 
+	eg.Go(func() error {
 		for {
 			if sConn != nil {
 				n, err := io.Copy(cConn, sConn)
@@ -170,24 +237,27 @@ func (p *Proxy) Start(ctx context.Context, cConn *net.TCPConn) error {
 					return err
 				}
 
-				if 0 < n {
+				if n < 0 {
 					return io.EOF
 				}
 			}
 
 			select {
-			case <- ctx.Done():
+			case <- closed.Done():
 				return nil
 			default:
-				continue
 			}
 		}
+
+
 	})
 
 	err := eg.Wait()
+	cConn.Close()
+	cConn = nil
 	if sConn != nil {
-	  sConn.Close()
+		sConn.Close()
 	}
-
+	
 	return err
 }
